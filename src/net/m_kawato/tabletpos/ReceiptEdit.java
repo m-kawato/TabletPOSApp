13536bf60336332b8e0a7d63b77d88e910375d89
Masahiro Kawato <masahiro.kawato@gmail.com>
2013-11-24 22:13:25 +0900

    Add navigation button to ReceiptEdit activity (issue #59)

--- src/net/m_kawato/tabletpos/ReceiptEdit.java
+++ src/net/m_kawato/tabletpos/ReceiptEdit.java
@@ -2,14 +2,27 @@ package net.m_kawato.tabletpos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class ReceiptEdit extends Activity {
+public class ReceiptEdit extends Activity implements OnClickListener {
+    private Globals globals;
+    private static final String TAG = "ReceiptEdit";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt_edit);
+
+        this.globals = (Globals) this.getApplication();
+
+        // Event handler of button (Top Menu)
+        Button btnTopMenu = (Button) findViewById(R.id.btn_topmenu);
+        btnTopMenu.setOnClickListener(this);   
     }
 
     @Override
@@ -17,6 +30,21 @@ public class ReceiptEdit extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.receipt_edit, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+        case R.id.btn_topmenu:
+            Log.d(TAG, "Top Menu button is clicked");
+            globals.saveLoading();
+            i = new Intent(this, TopMenu.class);
+            startActivity(i);
+            break;
+        case R.id.btn_loading:
+        }                
     }
 
 }
